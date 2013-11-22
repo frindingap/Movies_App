730d9c1ebd24c29814691acd075d76416ed9df01
Alberto Blanco <albertoblanco.s@gmail.com>
2013-11-21 18:24:46 -0800

    Finish Movies App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoviesApp::Application.config.secret_key_base = '14881207f4a3ebdbecee244afa34c04bff6ab8ecc8c0a3af142f51749dca019c7221f6f2a24b849af364933649db31f498f649a74578b777fff2c40f4b16f71a'
